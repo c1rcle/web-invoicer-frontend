abc7584bb22ad846409b80803b6a8f1d8654edd5
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-10-23 00:15:44 +0200

    Updates

---
+++ precache-manifest.e61f0d01a9788a68dafb55f1ab83bdc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2437fe9eec33245a8f67f869646957",
+    "url": "/web-invoicer-frontend/index.html"
+  },
+  {
+    "revision": "abbe04f0cc05351801b7",
+    "url": "/web-invoicer-frontend/static/css/main.15382792.chunk.css"
+  },
+  {
+    "revision": "49cdc8be17fdc36c0c0f",
+    "url": "/web-invoicer-frontend/static/js/2.1bd6c65f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/web-invoicer-frontend/static/js/2.1bd6c65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbe04f0cc05351801b7",
+    "url": "/web-invoicer-frontend/static/js/main.930cd7b7.chunk.js"
+  },
+  {
+    "revision": "2c4478536d5c950c6107",
+    "url": "/web-invoicer-frontend/static/js/runtime-main.956f2cac.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/web-invoicer-frontend/static/media/getFetch.013f0b90.cjs"
+  }
+]);
