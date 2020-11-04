b8e3a3876acbc1c4eb715fccc709b71e4bf598bb
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-11-04 01:37:57 +0100

    Created the data error dialog component

---
+++ src/components/common/ErrorDialog.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from '@material-ui/core';
+
+const ErrorDialog = ({ error, clearAction }) => {
+  const [displayedError, setDisplayedError] = useState('');
+
+  const { t } = useTranslation();
+
+  const dispatch = useDispatch();
+
+  const closeDialog = () => {
+    dispatch(clearAction(null));
+  };
+
+  const getTitle = () => (displayedError ? t(`tableError.${displayedError}`) : '');
+
+  const getText = () =>
+    t(`tableError.text.${displayedError === 'details' ? 'details' : 'serverDown'}`);
+
+  useEffect(() => {
+    error && setDisplayedError(error);
+  }, [error]);
+
+  return (
+    <Dialog open={Boolean(error)} onClose={closeDialog}>
+      <DialogTitle>{getTitle()}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{getText()}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={closeDialog} color='primary' autoFocus>
+          {t('cancel')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ErrorDialog;
