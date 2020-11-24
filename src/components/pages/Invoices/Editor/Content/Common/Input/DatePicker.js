025790b6008c25ed27765bfb5a9855dd46d15c66
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-11-24 13:09:07 +0100

    Reduced bundle size

--- src/components/pages/Invoices/Editor/Content/Common/Input/DatePicker.js
+++ src/components/pages/Invoices/Editor/Content/Common/Input/DatePicker.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import DateFnsUtils from '@date-io/date-fns';
-import * as locales from 'date-fns/locale';
+import enLocale from 'date-fns/locale/en-GB';
+import plLocale from 'date-fns/locale/pl';
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+
+const locales = {
+  en: enLocale,
+  pl: plLocale
+};
 
 const DatePicker = ({ onChange, minDate, ...props }) => {
   const { i18n } = useTranslation();
